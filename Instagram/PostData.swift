260d588f44fef0cc9a2a2bad7dc9effc573cd4dd
snowmania0445 <sunoharatky@gmail.com>
2018-04-19 11:53:05 +0900

    initial commit 課題前

---
+++ Instagram/PostData.swift
@@ -0,0 +1,49 @@
+//
+//  PostData.swift
+//  Instagram
+//
+//  Created by 春原卓也 on 2018/04/18.
+//  Copyright © 2018年 takuya.sunohara. All rights reserved.
+//
+
+import Firebase
+import FirebaseDatabase
+import UIKit
+
+class PostData: NSObject {
+    var id: String?
+    var image: UIImage?
+    var imageString: String?
+    var name: String?
+    var caption: String?
+    var date: Date?
+    var likes: [String] = []
+    var isLiked: Bool = false
+    
+    init(snapshot: DataSnapshot, myId: String) {
+        self.id = snapshot.key
+        
+        let valueDictionary = snapshot.value as! [String: Any]
+        
+        imageString = valueDictionary["image"] as? String
+        image = UIImage(data: Data(base64Encoded: imageString!, options: .ignoreUnknownCharacters)!)
+        
+        self.name = valueDictionary["name"] as? String
+        
+        self.caption = valueDictionary["caption"] as? String
+        
+        let time = valueDictionary["time"] as? String
+        self.date = Date(timeIntervalSinceReferenceDate: TimeInterval(time!)!)
+        
+        if let likes = valueDictionary["likes"] as? [String] {
+            self.likes = likes
+        }
+        
+        for likeId in self.likes {
+            if likeId == myId {
+                self.isLiked = true
+                break
+            }
+        }
+    }
+}
